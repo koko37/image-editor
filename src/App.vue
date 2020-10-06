6e589a4f57b2b660c54eab20e567d0e01e36a16b
wjone <engel0088@gmail.com>
2020-10-06 09:29:36 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1 class="text-lg min-h-screen text-red-500 p-4">Hello world</h1>
+  </div>
+</template>
+
+<script>
+import './assets/styles/index.scss';
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#app {
+  @apply bg-gray-300;
+}
+</style>
